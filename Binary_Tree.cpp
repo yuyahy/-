9f02c9fbb60f1b598bb49d0004a85a9df9aabcb2
yuyahy <xvto.ya@gmail.com>
2020-07-08 22:05:53 +0900

    [プログラミングコンテスト攻略のためのアルゴリズムとデータ構造] 第8章 木-二分木(ロジックはOK,Segmentation fault 11)

--- Binary_Tree.cpp
+++ Binary_Tree.cpp
@@ -23,26 +23,40 @@ void rec(int node,int parent){
         // 子に自分の深さ+1を記憶させる
         rec(iNode[node].left,parent + 1);
         rec(iNode[node].right,parent + 1);
+    } else if (((iNode[node].right == NIL) && (iNode[node].left != NIL))) {
+        rec(iNode[node].left,parent + 1);
+    } else if (((iNode[node].right != NIL) && (iNode[node].left == NIL))) {
+        rec(iNode[node].right,parent + 1);
     } else {
         if (parent > Depth_MAX) Depth_MAX = parent;
     }
 }
 
 // 再帰的に高さを求める
-int Rec_Height(int node, int parent,int cnt_right,int cnt_left) {
-    int cnt_right_tmp = cnt_right;
-    int cnt_left_tmp = cnt_left;
+int Rec_Height(int node) {
+    int cnt_right, cnt_left,cnt_height;
+    cnt_height = cnt_right = cnt_left = 0;
+    int idx_Parent = iNode[node].parent;
+    //cout << "node " << node << " " << cnt_left << " " << cnt_right << "\n";
+
     // 末端についたら終了
     if(iNode[node].right == NIL && iNode[node].left == NIL) {
         Height[node] = 0;
+        //cout << "末端" << node << "\n"; 
+        //Height[idx_Parent] = 1;
         return 1;
     }
     // それ以外の場合は再帰
     else {
-        cnt_left += Rec_Height(iNode[node].left, parent, cnt_right,cnt_left);
-        cnt_right += Rec_Height(iNode[node].right, parent, cnt_right,cnt_left);
+        cnt_right = Rec_Height(iNode[node].left);
+        cnt_left = Rec_Height(iNode[node].right);
     }
-    Height[node] = cnt_left>cnt_right? cnt_left : cnt_right;
+    //cout << "node " << node << " " << cnt_left <<" " << cnt_right << "\n";
+    cnt_height = cnt_left>cnt_right? cnt_left : cnt_right;
+    //cout << "idx_parent " << idx_Parent << " node " << node << "cnt_height " << cnt_height <<"\n"; 
+    
+    Height[node] = cnt_height;
+    return 1 + cnt_height;
 }
 
 // 与えられたノードの兄弟を返す
@@ -56,10 +70,12 @@ int GetBrother(int node) {
         iParent = iNode[node].parent;
         // 親の子で自分ではない方が兄弟
         // 二分木なので左右だけチェックすればOK
-        if(iNode[iParent].left == node) {
+        if(iNode[iParent].left == node && iNode[iParent].right != NIL) {
             return iNode[iParent].right;
-        } else {
+        } else if(iNode[iParent].right == node && iNode[iParent].left != NIL) {
             return iNode[iParent].left;
+        } else {
+            return NIL;
         }
     }
 }
@@ -101,26 +117,29 @@ int main() {
     // 記憶していく
     for (int i = 0; i < n; i++)
     {
-        // 節点番号を取得
+        //節点番号を取得
         cin >> iNodeIdx;
-        // 全ての子の情報を記憶する
+        //全ての子の情報を記憶する
         for (int k = 0; k < BINARY_CHILDREN_NUM; k++)
         {
             cin >> node;
+            //if (node == NIL) continue;
             if (k == 0)
             {
-                // 左の子を記憶
+                //左の子を記憶
                 iNode[iNodeIdx].left = node;
             }
             else
             {
-                // 右の子を記憶
+                //右の子を記憶
                 iNode[iNodeIdx].right = node;
             }
-            // 子の親を記憶する
+            //子の親を記憶する
             iNode[node].parent = iNodeIdx;
+            cout << "ここまで " << n <<  " k " << k << " iNodeIdx " << iNodeIdx << endl;
         }
     }
+
     // rootを探す
     for (int i = 0; i < n; ++i)
     {
@@ -134,7 +153,7 @@ int main() {
     rec(root, Depth_MAX);
 
     // 根からはじめて高さを代入する
-    Rec_Height(root, Depth_MAX, 0, 0);
+    Rec_Height(root);
 
     // Nodeのindexごとに出力する
     for (int i = 0; i < n; ++i)
