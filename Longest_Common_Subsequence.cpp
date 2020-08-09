ca97bd851c59450cbbce63c479d1478980820d17
yuyahy <xvto.ya@gmail.com>
2020-08-09 21:35:38 +0900

    [プログラミングコンテスト攻略のためのアルゴリズムとデータ構造] 第11章 動的計画法-最長共通部分列(途中)

--- Longest_Common_Subsequence.cpp
+++ Longest_Common_Subsequence.cpp
@@ -1,24 +1,88 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define MAX 2000
+#define NOTFOUND -1
+int Max = 0;
+// nまでの文字列をいくつか選んで最長共通部を作ったときの長さ
+//int dp[MAX];
 
-int SearchCommonSubsequence(string strSet1, string strSet2)
+bool 
+IsInTargetString(string strFind, string strTgt)
 {
-    map<std::string, bool> mapSearchedStrings;
-    // 
-    while(true) {
-        //strSet1
-    }
+    int idx;
+    string strTmp = strTgt;
 
-    pair<std::string, bool> regPair;
-
-    for(int i=0; i<strSet1.size(); i++) {
-        if(strSet2.find(strSet1[i]) != string::npos) {
-            regPair = make_pair(strSet1[i], true);
+    for (int i = 0; i < strFind.size(); i++)
+    {
+        idx = strTgt.find(strFind[i]);
+        if (idx != string::npos) {
+            strTmp = strTgt.substr(idx+1);
+            // cout << "idx = " << idx << " strFind = " << strFind
+            // << " strTmp = " << strTmp <<  "\n";
         } else {
-            regPair = make_pair(strSet1[i], false);
+            return false;
         }
-        mapSearchedStrings.insert(regPair);
+        strTgt = strTmp;
     }
+    //cout <<  "\n";
+    return true;
+}
+
+int
+SearchCommonSubsequence(string strSet1, string strSet2)
+{
+    vector<vector<bool>> dp(MAX, vector<bool>(MAX));
+    //vector<int> dp(MAX);
+    string strRes = "";
+
+    // nまでの文字列を
+    for (int k = 0; k < strSet1.size()-1; k++)
+    {
+        if (strSet2.find(strSet1[k]) != string::npos) {
+            dp[k][0] = true;
+        } else {
+            strRes = string(1, strSet1[k]);
+            dp[k][0] = false;
+            continue;
+        }
+
+        string strTmp = strSet2;
+        int tmpMax = 0;
+        for(int i=k+1; i<strSet1.size(); i++) {
+            int idx = strTmp.find(strSet1[i]);
+            if(dp[k][i-1] && idx != string::npos) {
+                dp[k][i] = true;
+                strTmp = strTmp.substr(idx+1);
+                tmpMax++;
+                cout << "tmpMax = " << tmpMax << endl;
+            } else {
+                // 見つからなかった場合はそのまま
+                break;
+            }
+        //cout << strRes << ", " << strSet1[i] << "\n";
+         //dp[k][i] = strRes.size();
+        }
+        Max = max(tmpMax, Max);
+    }
+    
+    // for(int i=0; i<strSet1.size(); i++) {
+    //     string strTmp = strRes;
+    //     strTmp += strSet1[i];
+
+    //     //if(strSet2.find(strTmp) != string::npos) {
+    //     if(IsInTargetString(strTmp, strSet2)) {
+    //         // 見つかった場合は更新a
+    //         strRes = strTmp;
+    //     } else {
+    //         // 見つからなかった場合はそのまま
+    //     }
+    //     dp[i] = strRes.size();
+    //     //cout << strRes << ", " << strSet1[i] << "\n";
+    // }
+
+    //auto itr = std::max_element(dp.begin(), dp.end());
+    //return *itr;
+    return Max;
 }
 
 int main()
@@ -29,9 +93,10 @@ int main()
     cin >> n;
     for (int i = 0; i < n; i++)
     {
-        cin >> strSet1;
-        cin >> strSet2;
-    }
-    
+        cin >> strSet1 >> strSet2;
+
+        if(strSet1.size() > strSet2.size()) swap(strSet1, strSet2);
+        cout << SearchCommonSubsequence(strSet1, strSet2) << "\n";
+    }    
     return 0;
 }
