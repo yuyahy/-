4136e1a61d473034d914156651c711003d39337a
yuyahy <xvto.ya@gmail.com>
2020-08-10 12:25:39 +0900

    [プログラミングコンテスト攻略のためのアルゴリズムとデータ構造] 第11章 動的計画法-最長共通部分列(完了)

--- Longest_Common_Subsequence.cpp
+++ Longest_Common_Subsequence.cpp
@@ -1,88 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define MAX 2000
-#define NOTFOUND -1
-int Max = 0;
-// nまでの文字列をいくつか選んで最長共通部を作ったときの長さ
-//int dp[MAX];
 
-bool 
-IsInTargetString(string strFind, string strTgt)
-{
-    int idx;
-    string strTmp = strTgt;
-
-    for (int i = 0; i < strFind.size(); i++)
-    {
-        idx = strTgt.find(strFind[i]);
-        if (idx != string::npos) {
-            strTmp = strTgt.substr(idx+1);
-            // cout << "idx = " << idx << " strFind = " << strFind
-            // << " strTmp = " << strTmp <<  "\n";
-        } else {
-            return false;
-        }
-        strTgt = strTmp;
-    }
-    //cout <<  "\n";
-    return true;
-}
-
+// 与えられた2つの文字列の最長共通部分列(LCS)を求める
 int
 SearchCommonSubsequence(string strSet1, string strSet2)
 {
-    vector<vector<bool>> dp(MAX, vector<bool>(MAX));
-    //vector<int> dp(MAX);
+    // dp[i+1][j+1] = strSet1のi 文字目までとstrSet2のj文字目まででの LCS の長さ
+    vector<vector<int>> dp(MAX, vector<int>(MAX));
+    dp[0][0] = 0;
     string strRes = "";
 
-    // nまでの文字列を
-    for (int k = 0; k < strSet1.size()-1; k++)
+    // 以下の場合が考えられる
+    // 1. strSet1[i] = strSet2[j]の場合→dp[i][j]にstrSet1[i] = strSet2[j]の文字を加えると長さが+1される
+    // 2. strSet1[i] != strSet2[j]の場合→dp[i+1][j],dp[i][j+1]の大きい方をそのまま採用する(長さは増えない)
+    for (int i = 0; i < strSet1.size(); i++)
     {
-        if (strSet2.find(strSet1[k]) != string::npos) {
-            dp[k][0] = true;
-        } else {
-            strRes = string(1, strSet1[k]);
-            dp[k][0] = false;
-            continue;
-        }
-
-        string strTmp = strSet2;
-        int tmpMax = 0;
-        for(int i=k+1; i<strSet1.size(); i++) {
-            int idx = strTmp.find(strSet1[i]);
-            if(dp[k][i-1] && idx != string::npos) {
-                dp[k][i] = true;
-                strTmp = strTmp.substr(idx+1);
-                tmpMax++;
-                cout << "tmpMax = " << tmpMax << endl;
+        for (int j = 0; j < strSet2.size(); j++)
+        {
+            if(strSet1[i] == strSet2[j]) {
+                dp[i+1][j+1] = max({dp[i][j] + 1,dp[i+1][j], dp[i][j+1]});
             } else {
-                // 見つからなかった場合はそのまま
-                break;
+                dp[i+1][j+1] = max(dp[i+1][j], dp[i][j+1]);
             }
-        //cout << strRes << ", " << strSet1[i] << "\n";
-         //dp[k][i] = strRes.size();
         }
-        Max = max(tmpMax, Max);
     }
-    
-    // for(int i=0; i<strSet1.size(); i++) {
-    //     string strTmp = strRes;
-    //     strTmp += strSet1[i];
-
-    //     //if(strSet2.find(strTmp) != string::npos) {
-    //     if(IsInTargetString(strTmp, strSet2)) {
-    //         // 見つかった場合は更新a
-    //         strRes = strTmp;
-    //     } else {
-    //         // 見つからなかった場合はそのまま
-    //     }
-    //     dp[i] = strRes.size();
-    //     //cout << strRes << ", " << strSet1[i] << "\n";
-    // }
-
-    //auto itr = std::max_element(dp.begin(), dp.end());
-    //return *itr;
-    return Max;
+    return dp[strSet1.size()][strSet2.size()];
 }
 
 int main()
